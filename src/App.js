c53580087a90c08f37b4e28fdde482b1465dda21
kha.pham@dssolution.jp <kha.pham@dssolution.jp>
2024-07-01 05:15:20 +0700

    Fix page title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import MonsterTable from './table/monsterTable';
 import monsterDatas from './data.json';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = 'PDQ - P&D Quicksearch';
+  }, []);
   return (
     <div className="App">
       <MonsterTable data={monsterDatas}/>
